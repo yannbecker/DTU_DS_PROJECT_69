32425f919480c2b3af764a6ed96314d1029f2a65
Thibaut Heim <thibaut.heim@etu.ec-lyon.fr>
2025-11-29 15:40:08 +0100

    Refactor LSH implementation and Jaccard similarity functions
    - Updated execution counts in Implementing_lsh.ipynb for better tracking.
    - Changed output file naming convention in Implementing_lsh.ipynb for clarity.
    - Added new functions for computing Jaccard similarity based on shingles and signatures in Jaccard_similarity.py.
    - Modified prepro.ipynb to improve JSON handling and execution counts, ensuring consistent output messages.
    - Updated kernel specifications in prepro.ipynb for environment consistency.

--- LSH/lsh.py
+++ LSH/lsh.py
@@ -34,7 +34,7 @@ def preprocess_lsh(dataset_path):
 
 # Implementing lsh function         
 
-def lsh(input, signature_matrix, idx_to_id, m, shingle_size, nb_band, band_size):
+def lsh(input, article_list ,signature_matrix, idx_to_id, m, shingle_size, nb_band, band_size):
     """
     Inputs :
         input : input text from which we want to obtain sources
@@ -102,8 +102,9 @@ def lsh(input, signature_matrix, idx_to_id, m, shingle_size, nb_band, band_size)
     Ordered_similar_candidates = similar_candidates.keys()
     Ordered_similarities = []
     for idx in tqdm(similar_candidates, desc="Calculating Similarities"):
-        j = Jaccard_similarity(input_signature = input_signature, 
-                               doc_signature = signature_matrix[:,idx])
+        j = Jaccard_similarity(input_text= input, 
+                               article_list= article_list,
+                               candidate_index=idx)
         Ordered_similarities.append(j)
     Most_similar = zip(Ordered_similar_candidates,Ordered_similarities)
     Most_similar = sorted(Most_similar, key = lambda pair:pair[1], reverse = True)
@@ -112,40 +113,6 @@ def lsh(input, signature_matrix, idx_to_id, m, shingle_size, nb_band, band_size)
     
     return Most_similar, Scores
 
-if __name__ == "__main__" :
 
-    json_path_Nmost = 'DTU_DS_PROJECT_69/data/processed/filtered_articles_Nmostcited.json'
-
-    data_Nmost = preprocess_lsh(dataset_path = json_path_Nmost)
-    print("preprocessing done")
-
-    try :
-        np.save(file = f"DTU_DS_PROJECT_69/data/processed/signatures_lsh/idx_id_Nmost_q{q}_b{b}_r{r}", arr = signature_matrix_Nmost)
-    except Exception :
-        print("echec")
-
-    q = 7
-    b = 4
-    r = 5
-
-    signature_matrix_Nmost, idx_to_id_Nmost = signatures(
-        doc_list=data_Nmost,
-        shingle_size = q,
-        signature_size = b*r
-        )
-    
-    np.save(file = f"DTU_DS_PROJECT_69/data/processed/signatures_lsh/Nmost_q{q}_b{b}_r{r}", arr = signature_matrix_Nmost)
-    np.save(file = f"DTU_DS_PROJECT_69/data/processed/signatures_lsh/idx_id_Nmost_q{q}_b{b}_r{r}", arr = signature_matrix_Nmost)
-
-    Most_similar, Scores = lsh(
-        input = data_Nmost[0]['abstract'],
-        signature_matrix = signature_matrix_Nmost,
-        idx_to_id = idx_to_id_Nmost,
-        shingle_size = q,
-        nb_band = b,
-        band_size = r,
-        )
-    
-    print("Most similar documents : ", Most_similar, '\n', "Jaccard similarity scores : ", Scores)
 
 
--- LSH/lsh_utils/Jaccard_similarity.py
+++ LSH/lsh_utils/Jaccard_similarity.py
@@ -1,6 +1,6 @@
-# Jaccard similarity
+from lsh_utils.shingle import shingle
 
-def Jaccard_similarity(input_signature, doc_signature) -> float :
+def Jaccard_similarity_signatures(input_signature, doc_signature) -> float :
     "Returns an approximation of the jaccard similarity between 2 documents doc_name1 and doc_name2 using signatures"
     sig = doc_signature 
     S = 0
@@ -44,3 +44,9 @@ def Jaccard_similarity_shingles(shingles_list_A, shingles_list_B):
 
     jaccard_score = intersection_size / union_size
     return jaccard_score
+
+
+def Jaccard_similarity(input_text ,article_list, candidate_idx, q):
+    return Jaccard_similarity_shingles(
+        shingles_list_A= shingle(q = q, text = input_text),
+        shingles_list_B= shingle(q = q, text = article_list[candidate_idx]["clean_text"]))
